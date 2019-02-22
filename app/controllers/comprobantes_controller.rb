41d84a083eb8d6b6480f4a90ab824af7a5519e0d
JuanCalderon <Pest1lozzi>
2019-02-22 16:10:51 -0700

    Verificar que el PDF existe en comprobantes

--- app/controllers/comprobantes_controller.rb
+++ app/controllers/comprobantes_controller.rb
@@ -3,10 +3,15 @@ class ComprobantesController  < ApplicationController
   def pdf
 
     folio = params[:folio] #.to_i(32) # eg PAYA170380
-    send_file(
-        "#{Rails.root}/private/facturas/#{folio}.pdf",
+    file_path = "#{Rails.root}/private/facturas/#{folio}.pdf"
+    if File.exist?(file_path)
+      send_file(
+          file_path,
         filename: "#{folio}.pdf", type: "application/pdf", disposition: :inline
-    )
+      )
+    else
+      render :plain => "PDF #{folio} no existe"
+    end
 
   end
 
