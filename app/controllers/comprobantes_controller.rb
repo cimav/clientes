6d41c988d501c61839a40565f3e58d24735b9927
JuanCalderon <Pest1lozzi>
2019-02-22 15:58:27 -0700

    PDFs de comprobantes de viáticos

--- app/controllers/comprobantes_controller.rb
+++ app/controllers/comprobantes_controller.rb
@@ -2,10 +2,10 @@ class ComprobantesController  < ApplicationController
 
   def pdf
 
-    folio = params[:folio] #.to_i(32)
+    folio = params[:folio] #.to_i(32) # eg PAYA170380
     send_file(
-        "#{Rails.root}/private/facturas/P#{folio}.pdf",
-        filename: "P#{folio}.pdf", type: "application/pdf", disposition: :inline
+        "#{Rails.root}/private/facturas/#{folio}.pdf",
+        filename: "#{folio}.pdf", type: "application/pdf", disposition: :inline
     )
 
   end
